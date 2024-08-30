bd70c7da669c7e3fa0d2d67ccfd268361a777cea
Hamid Rafique <hamidch0866@gmail.com>
2024-08-30 11:44:16 +0500

    Encapsolation

---
+++ Encapsolation.js
@@ -0,0 +1,32 @@
+class BankAccount 
+{
+    #balance; // this the variable that can not be accessible out of class meand encapsole
+
+    constructor(balance) {
+        this.#balance= balance
+    }
+    deposit(amount)
+    {
+        this.#balance += amount
+    }
+    Withdraw(amount)
+    {
+        if(amount <= this.#balance)
+        {
+            this.#balance-= amount
+        }
+        else
+        {
+            console.log("Amount unsufficient ");
+        }
+    }
+    Getbalance()
+    {
+        return this.#balance
+    }
+}
+const Account = new BankAccount(1000)
+Account.deposit(8000)
+console.log(Account.Getbalance());
+Account.Withdraw(2000)
+console.log(Account.Getbalance());
